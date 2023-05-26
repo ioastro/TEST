409404f2aaafc2c358502e7b17dbff1ac0768ca0
IoannisGkanatsios <ioannisgkan259@gmail.com>
2023-05-26 15:17:47 +0100

    another change

--- another_example.py
+++ another_example.py
@@ -19,5 +19,17 @@ def padding(json,image):
     return new_img
 
 
+    img_arr =np.array(Image.open(image))
+    a = int(points[0,0])
+    b = int(points[0,1])
+    c = int(points[1,0])
+    d = int(points[1,1])
+    img_arr[0:a,:,:] = 0
+    img_arr[:, 0:b, :] = 0
+    img_arr[c:649, :, :] = 0
+    img_arr[:, d:649, :] = 0
+    new_img = Image.fromarray(img_arr)
+    return new_img
+
 
 
